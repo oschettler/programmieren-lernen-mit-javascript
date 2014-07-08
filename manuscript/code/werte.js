7e64a1db5eaaa1a026315b3e6ddc447cc3aed502
Olav Schettler <olav@pixelhouse.de>
2014-07-08 21:10:49 +0200

    Werte

---
+++ manuscript/code/werte.js
@@ -0,0 +1,37 @@
+// Zahlen = 3.6
+-2.4 + 6
+
+// Zeichenketten = "Lena1"
+"Lena" + 1
+
+// Zeichenketten = "-26"
+"-2" + 6
+
+// Zahlen = 4
+parseInt("-2") + 6
+
+// Feld
+var liste = [2, 3, "vier", [5, 6]]
+
+// 1. Element = 2
+liste[0]
+
+// 3. Element = "vier"
+liste[2]
+
+// 4. Element = [5, 6]
+
+// Objekt
+var paul = { name: "Paul", wohntIn: [53111, "Bonn"] }
+
+// Element "name" = "Paul"
+paul.name
+
+// alternativ: Element "name" = "Paul"
+paul['name']
+
+// 2. Element in "wohntIn" = "Bonn"
+paul.wohntIn[1]
+
+// alternativ: 2. Element in "wohntIn" = "Bonn"
+paul['wohntIn'][1]
