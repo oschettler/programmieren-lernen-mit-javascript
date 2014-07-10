02372f21d363f25bfc3a6659836352d6bea79470
Olav Schettler <olav@pixelhouse.de>
2014-07-10 22:13:22 +0200

    Einige Typos

--- manuscript/code/berechnungen.js
+++ manuscript/code/berechnungen.js
@@ -1,17 +1,17 @@
 // Ergebnis: 20
-(2 + 3) * 4
+(2 + 3) * 4;
 
 // Punkt vor Strichrechnung. Ergebnis: 14
-2 + 3 * 4
+2 + 3 * 4;
 
 // Zahl um drei erhöhen. Ergebnis: 8
 var zahl = 5;
-zahl += 3
+zahl += 3;
 
 // Text verlängern. Ergebnis: "Lena lacht"
 var name = "Lena";
-name += " lacht"
+name += " lacht";
 
 // Zahl um eins herunterzählen. Ergebnis: -2
 var zahl = -1;
-zahl--
+zahl--;
